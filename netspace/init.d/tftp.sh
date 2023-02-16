fb7f981a62f0c3c0d005f106ba1da4d3062dd3cf
Patrik Nordvall <patrik.nordvall95@gmail.com>
2023-02-16 06:56:40 +0100

    use /run/dsnmasq.pid and create /srv/tftp when started
    Signed-off-by: Patrik Nordvall <patrik.nordvall95@gmail.com>

--- netspace/init.d/tftp.sh
+++ netspace/init.d/tftp.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-$START /usr/sbin/in.tftpd --listen --user tftp --address :69 --secure /tftpboot
+mkdir -p /srv/tftp
+chown root:tftp /srv/tftp
+$START /usr/sbin/in.tftpd --listen --user tftp --address :69 --secure /srv/tftp
